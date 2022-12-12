d8ec39284efd67da1c5aeb679999306c5e9f02ba
Your Name <you@example.com>
2022-12-12 23:49:07 +0300

    DI

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Music music = context.getBean("musicBean",Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic();
+        context.close();
+    }
+}
---
+++ src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ClassicalMusic implements Music {
+    public String getSong() {
+        return "9-я Симфония Моцарта";
+    }
+}
