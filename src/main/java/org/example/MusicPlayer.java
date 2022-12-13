ff8711902006ed1bdcaaae5472f722cc9e4d6ba6
Your Name <you@example.com>
2022-12-13 10:04:38 +0300

    dependency injection via setter

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,8 +3,12 @@ package org.example;
 public class MusicPlayer {
     private Music music;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+
+  //  public MusicPlayer(Music music) { // <constructor-arg ref="musicBean"/> внедрение через конструктор в аppContext тег
+    //    this.music = music;
+    //}
+    public void setMusic(Music music) { //внедрение через сеттер
+        this.music = music;             // важно название в теге и название сеттера music
     }
 
     public void playMusic() {
