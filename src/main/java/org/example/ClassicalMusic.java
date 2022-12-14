cbab45da693124a6c249713a0c111c5d3d0d4d94
Your Name <you@example.com>
2022-12-14 23:34:25 +0300

    adding methods init and deatroy

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class ClassicalMusic implements Music {
+    public void doMyInit(){
+        System.out.println("1-ым сработал метод инициализации и вывелся на печать текст");
+    }
+    public void doMyDestroy(){
+        System.out.println("Выше сработал бин , а сейчас распечатался метот destroy");
+    }
     public String getSong() {
         return "9-я Симфония Моцарта";
     }
